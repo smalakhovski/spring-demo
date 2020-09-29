fa66f3202397c6dd87c5774deb8a5b6c487b56aa
Sergei M <sergeim@us.ibm.com>
2020-09-29 03:15:04 +0300

    initial comit

---
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class Controller {
+
+    @RequestMapping("/names/{name}")
+    public String demoTest(@PathVariable String name) {
+
+        return "Hello " + name;
+
+    }
+
+}
