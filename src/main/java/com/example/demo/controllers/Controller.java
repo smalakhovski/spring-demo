903b3161a9881af9af3c01f7331c63fd6787de1e
Sergei M <sergeim@us.ibm.com>
2021-02-10 04:18:10 +0300

    git lesson 3 local commit

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -13,6 +13,7 @@ public class Controller {
 
         System.out.println("This is a Git Lesson 1");
         System.out.println("This is a Git Lesson 2");
+        System.out.println("This is a Git Lesson 3 local");
 
         return "Hello Hello Hello " + name;
 
