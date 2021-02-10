d5024db17c5a29d3821930cb9ae853fe531207d2
Sergei M <sergeim@us.ibm.com>
2021-02-10 03:17:28 +0300

    Lesson 1 commit

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -11,6 +11,8 @@ public class Controller {
     @RequestMapping("/names/{name}")
     public String demoTest(@PathVariable String name) {
 
+        System.out.println("This is a Git Lesson 1");
+
         return "Hello Hello Hello " + name;
 
     }
